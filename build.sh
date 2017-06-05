67be264e37a7b31299eb925af46e7d2fab744505
Stefan Gangefors <stefan@gangefors.se>
2017-06-05 16:06:57 +0200

    Update build script
    With no option it will build the latest version according to the
    Dockerfile or it will build the version given as first option.

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
-#!/usr/bin/env sh
-sudo docker build --pull -t gangefors/airdcpp-webclient:$VERSION --build-arg VERSION=$VERSION .
-sudo docker tag gangefors/airdcpp-webclient:$VERSION gangefors/airdcpp-webclient:latest
+#!/bin/bash
+if [ -z $1 ]; then
+    sudo docker build --no-cache --pull -t gangefors/airdcpp-webclient:latest .
+else
+    sudo docker build --no-cache --pull -t gangefors/airdcpp-webclient:$1 --build-arg VERSION=$1 .
+    sudo docker tag gangefors/airdcpp-webclient:$1 gangefors/airdcpp-webclient:latest
+fi
