d087246c9103e44ef0cdcf5486a45dc4dd942092
Stefan Gangefors <stefan@gangefors.se>
2017-08-25 19:49:13 +0200

    Remove sudo from build script

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ -z $1 ]; then
-    sudo docker build --no-cache --pull -t gangefors/airdcpp-webclient:latest .
+    docker build --no-cache --pull -t gangefors/airdcpp-webclient:latest .
 else
-    sudo docker build --no-cache --pull -t gangefors/airdcpp-webclient:$1 --build-arg VERSION=$1 .
-    sudo docker tag gangefors/airdcpp-webclient:$1 gangefors/airdcpp-webclient:latest
+    docker build --no-cache --pull -t gangefors/airdcpp-webclient:$1 --build-arg VERSION=$1 .
+    docker tag gangefors/airdcpp-webclient:$1 gangefors/airdcpp-webclient:latest
 fi
