c67a0f0fa2a99006f8666e2688fb51503822a14a
Stefan Gangefors <stefan@gangefors.se>
2017-08-25 19:47:10 +0200

    Update name of rpi image

--- rpi-build.sh
+++ rpi-build.sh
@@ -2,8 +2,8 @@
 # Set docker host to a Raspberry Pi's IP address
 export DOCKER_HOST=tcp://192.168.0.51:2376
 if [ -z $1 ]; then
-    docker build --no-cache --pull -f Dockerfile.rpi -t gangefors/arm32v7/airdcpp-webclient:latest .
+    docker build --no-cache --pull -f Dockerfile.rpi -t gangefors/arm32v7-airdcpp-webclient:latest .
 else
-    docker build --no-cache --pull -f Dockerfile.rpi -t gangefors/arm32v7/airdcpp-webclient:$1 --build-arg VERSION=$1 .
-    docker tag gangefors/arm32v7/airdcpp-webclient:$1 gangefors/arm32v7/airdcpp-webclient:latest
+    docker build --no-cache --pull -f Dockerfile.rpi -t gangefors/arm32v7-airdcpp-webclient:$1 --build-arg VERSION=$1 .
+    docker tag gangefors/arm32v7-airdcpp-webclient:$1 gangefors/arm32v7-airdcpp-webclient:latest
 fi
